8f4ef0cb9a6e20dc44ac6c7a1b0a29f2969fcf02
litao <358840429@qq.com>
2023-02-01 17:45:04 +0800

    init project

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/config/AppFilterConfig.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/config/AppFilterConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//跨域拦截器 
+//跨域拦截器
 public class AppFilterConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
